257e9c1a9b245183d3cad545b091613b2867af3f
Voogie James <voogie@origin.no>
2018-11-05 07:58:45 +0100

    Proceeding with configuring things

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello people!');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return (
+    <div>
+      <p>Hello I am a Webpack-React Hot-Pot!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
+
+module.hot.accept();
